9b49036b4b362a79af2d4c4f180a8b720042a830
pulketo <pulketo@gmail.com>
2017-12-22 09:57:04 -0600

    change getPrice name to Stocks.Exchange.getPrice, as we are getting prices from that api

--- start-here.sh
+++ start-here.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 php profit.php ITNS,XUN,BCN,XMR
-php getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC
+php Stocks.Exchange.getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC
--- start-new-terminal.sh
+++ start-new-terminal.sh
@@ -3,5 +3,5 @@
 #xterm -title 'Mining Profit @ 1KH/s' -geometry 80x10+0+0 -e 'watch -n60 ~/apis/profit.php ITNS,XUN,BCN'  &
 gnome-terminal --title 'Mining Profit @ 1KH/s' --hide-menubar --geometry=80x10+0+0 -x bash -c 'watch -n60 php profit.php ITNS,XUN,BCN,XMR'
 #sleep 5
-#xterm -title 'Stocks.Exchange Buy/Sell' -geometry 80x20+200+0 -e 'watch -n60 ~/apis/getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC' &
-gnome-terminal --title 'Stocks.Exchange Buy/Sell' --hide-menubar --geometry=80x20+0+220 -x bash -c 'watch -n60 php getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC'
+#xterm -title 'Stocks.Exchange Buy/Sell' -geometry 80x20+200+0 -e 'watch -n60 ~/apis/Stocks.Exchange.getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC' &
+gnome-terminal --title 'Stocks.Exchange Buy/Sell' --hide-menubar --geometry=80x20+0+220 -x bash -c 'watch -n60 php Stocks.Exchange.getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC'
