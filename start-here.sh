8678f11b4aacbf6a7476ea1065641a5207edb8cd
pulketo <pulketo@gmail.com>
2017-12-28 11:36:46 -0600

    fix filename and small fix on format_this

--- start-here.sh
+++ start-here.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-php profit.php ITNS,XUN,BCN,XMR
-php Stocks.Exchange.getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC
+php profit_per_hashsecond.php ITNS,XUN,BCN,XMR,DERO
+php Stocks.Exchange.getPrice.php ITNS_BTC,XUN_BTC,BCN_BTC,DERO_BTC
